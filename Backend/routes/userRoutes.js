814aae81db790839738a61f51b0a4971a2ed7aec
Shahmeer Arshad <ranashahmeerarshad@gmail.com>
2026-06-26 14:02:19 +0500

    Refactor follow routes to use toggleFollow

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,19 +1,14 @@
-
 import express from "express";
 import authMiddleware from "../middleware/authMiddleware.js";
-
 import {
   getUserProfile,
-  followUser,
-  unfollowUser,
+  toggleFollow,
 } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.get("/:id", authMiddleware, getUserProfile);
 
-router.put("/follow/:id", authMiddleware, followUser);
-
-router.put("/unfollow/:id", authMiddleware, unfollowUser);
+router.put("/:id/follow", authMiddleware, toggleFollow);
 
 export default router;
