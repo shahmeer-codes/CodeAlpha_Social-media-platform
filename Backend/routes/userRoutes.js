32f8c23e97873ecafc951dcfd4648d3e8872f0f7
Shahmeer Arshad <ranashahmeerarshad@gmail.com>
2026-06-26 13:57:52 +0500

    Update userRoutes.js

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,1 +1,19 @@
 
+import express from "express";
+import authMiddleware from "../middleware/authMiddleware.js";
+
+import {
+  getUserProfile,
+  followUser,
+  unfollowUser,
+} from "../controllers/userController.js";
+
+const router = express.Router();
+
+router.get("/:id", authMiddleware, getUserProfile);
+
+router.put("/follow/:id", authMiddleware, followUser);
+
+router.put("/unfollow/:id", authMiddleware, unfollowUser);
+
+export default router;
