8b7a198494c82d9ab611c6c57fe4b4fc788d4f8a
Shahmeer Arshad <ranashahmeerarshad@gmail.com>
2026-06-26 15:43:56 +0500

    Add user profile and follow toggle service functions

--- Frontend/src/services/userService.js
+++ Frontend/src/services/userService.js
@@ -1,1 +1,12 @@
 
+import axiosInstance from "./axiosInstance";
+
+export const getUserProfile = async (userId) => {
+  const { data } = await axiosInstance.get(`/users/${userId}`);
+  return data;
+};
+
+export const toggleFollow = async (userId) => {
+  const { data } = await axiosInstance.put(`/users/${userId}/follow`);
+  return data;
+};
