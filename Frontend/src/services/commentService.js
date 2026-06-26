b058c86fb3eb992b68089182d0d4156cdc50539f
Shahmeer Arshad <ranashahmeerarshad@gmail.com>
2026-06-26 15:42:32 +0500

    Add comment service functions for CRUD operations

--- Frontend/src/services/commentService.js
+++ Frontend/src/services/commentService.js
@@ -1,1 +1,20 @@
 
+import axiosInstance from "./axiosInstance";
+
+export const getComments = async (postId) => {
+  const { data } = await axiosInstance.get(`/comments/${postId}`);
+  return data;
+};
+
+export const createComment = async (postId, text) => {
+  const { data } = await axiosInstance.post(`/comments/${postId}`, {
+    text,
+  });
+
+  return data;
+};
+
+export const deleteComment = async (commentId) => {
+  const { data } = await axiosInstance.delete(`/comments/${commentId}`);
+  return data;
+};
