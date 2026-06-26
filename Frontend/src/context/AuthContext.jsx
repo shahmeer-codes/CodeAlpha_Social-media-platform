0a838710edb2c00b3730a1b0ab1a3f5b44b83684
Shahmeer Arshad <ranashahmeerarshad@gmail.com>
2026-06-26 14:17:58 +0500

    Add AuthContext for user authentication management
    Implement authentication context with login and logout functionality.

--- Frontend/src/context/AuthContext.jsx
+++ Frontend/src/context/AuthContext.jsx
@@ -1,1 +1,59 @@
 
+import { createContext, useContext, useEffect, useState } from "react";
+import axiosInstance from "../services/axiosInstance";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const token = localStorage.getItem("token");
+
+  useEffect(() => {
+    const getCurrentUser = async () => {
+      if (!token) {
+        setLoading(false);
+        return;
+      }
+
+      try {
+        const { data } = await axiosInstance.get("/auth/me");
+
+        setUser(data.user);
+      } catch (error) {
+        localStorage.removeItem("token");
+        setUser(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getCurrentUser();
+  }, []);
+
+  const login = (token, user) => {
+    localStorage.setItem("token", token);
+    setUser(user);
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        loading,
+        login,
+        logout,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
