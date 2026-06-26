8b8d44fa0422238437676060f826695b28492845
Shahmeer Arshad <ranashahmeerarshad@gmail.com>
2026-06-26 11:59:47 +0500

    Import authRoutes in server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-
+import authRoutes from "./routes/authRoutes.js";
 import connectDB from "./config/db.js";
 
 dotenv.config();
