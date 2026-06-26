0fd452cc1f10f2bf722987717fb092c401924e25
Shahmeer Arshad <ranashahmeerarshad@gmail.com>
2026-06-26 14:04:18 +0500

    Add comment routes with authentication middleware

--- Backend/routes/commentRoutes.js
+++ Backend/routes/commentRoutes.js
@@ -1,1 +1,19 @@
 
+import express from "express";
+import authMiddleware from "../middleware/authMiddleware.js";
+
+import {
+  createComment,
+  getComments,
+  deleteComment,
+} from "../controllers/commentController.js";
+
+const router = express.Router();
+
+router.get("/:postId", getComments);
+
+router.post("/:postId", authMiddleware, createComment);
+
+router.delete("/:id", authMiddleware, deleteComment);
+
+export default router;
