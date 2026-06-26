ad64b0f0421c8cffee24fdca30b42dae2e702669
Shahmeer Arshad <ranashahmeerarshad@gmail.com>
2026-06-26 15:40:17 +0500

    Implement post service functions for CRUD operations

--- Frontend/src/services/postService.js
+++ Frontend/src/services/postService.js
@@ -1,1 +1,35 @@
 
+import axiosInstance from "./axiosInstance";
+
+export const getPosts = async () => {
+  const { data } = await axiosInstance.get("/posts");
+  return data;
+};
+
+export const createPost = async (postData) => {
+  const formData = new FormData();
+
+  formData.append("content", postData.content);
+
+  if (postData.image) {
+    formData.append("image", postData.image);
+  }
+
+  const { data } = await axiosInstance.post("/posts", formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+
+  return data;
+};
+
+export const toggleLike = async (postId) => {
+  const { data } = await axiosInstance.put(`/posts/${postId}/like`);
+  return data;
+};
+
+export const deletePost = async (postId) => {
+  const { data } = await axiosInstance.delete(`/posts/${postId}`);
+  return data;
+};
