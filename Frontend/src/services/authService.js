4ed51162c18abcbc20466890ff1442d1d5c0b1be
Shahmeer Arshad <ranashahmeerarshad@gmail.com>
2026-06-26 15:39:18 +0500

    Update authService.js

--- Frontend/src/services/authService.js
+++ Frontend/src/services/authService.js
@@ -1,1 +1,17 @@
 
+import axiosInstance from "./axiosInstance";
+
+export const registerUser = async (userData) => {
+  const { data } = await axiosInstance.post("/auth/register", userData);
+  return data;
+};
+
+export const loginUser = async (userData) => {
+  const { data } = await axiosInstance.post("/auth/login", userData);
+  return data;
+};
+
+export const getCurrentUser = async () => {
+  const { data } = await axiosInstance.get("/auth/me");
+  return data;
+};
