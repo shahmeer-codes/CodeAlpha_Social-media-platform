20b944596dd4048bdca212d5dfb9f684329b5469
Shahmeer Arshad <ranashahmeerarshad@gmail.com>
2026-06-26 11:54:20 +0500

    Add Post model with schema and timestamps

--- Backend/models/Post.js
+++ Backend/models/Post.js
@@ -1,1 +1,32 @@
+import mongoose from "mongoose";
 
+const postSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 500,
+    },
+    image: {
+      type: String,
+      default: "",
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Post", postSchema);
